30a04a0e84034c5515197b678a6f03603750519c
Vaish-W <wanivaishnavi16@gmail.com>
2024-10-30 23:13:59 +0100

    Add calculate_difference function

--- calculate.py
+++ calculate.py
@@ -4,3 +4,7 @@ def calculate_total(a, b):
 
 if __name__ == "__main__":
     print("The total is:", calculate_total(10, 5))
+
+def calculate_difference(a, b):
+    """Calculate the difference between two numbers."""
+    return a - b
